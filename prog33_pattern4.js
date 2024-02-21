6472210ae91bd4e78ffe0b44bf71e63667dbc9c7
Amiyakiran <amiya.susy@gmail.com>
2024-02-21 15:30:11 +0530

    Update prog33_pattern4.js

--- prog33_pattern4.js
+++ prog33_pattern4.js
@@ -24,3 +24,26 @@ for(row=1;row<=4;row++){
 //           *   *  *
 //             *  *
 //               *
+
+
+for(row=1;row<=4;row++){//1<=4//2<=4
+    str=""//""//""
+    for(sp=4;sp>=row;sp--){//4>=2//3>=2//2>=2//1>=2
+        str= str+" "//####//###
+    }
+    for(col=1; col<=row;col++){//1<=2//2<=2//3<=2
+        str= str+' *'//*//** 
+    }
+    console.log(str);//####*//###**
+}
+for(row=0; row<=5; row++){
+    str = ''
+    for ( spa = 0; spa < row; spa++) {
+          str += " ";
+        }
+        // printing star
+        for (col = 4; col>=row; col--) {
+          str += " *";
+        }
+        console.log(str);
+ }    
