7acd3e11b6570ad45e884fa8d240e9037065acc5
Amiyakiran <amiya.susy@gmail.com>
2024-03-27 07:27:17 +0530

    Update prog34_pattern5.js
    added new pattern

--- prog34_pattern5.js
+++ prog34_pattern5.js
@@ -62,9 +62,21 @@ for(row=1;row<=4;row++){
 }
 
 // * * * * *
-//  *     *
 //   *  *
-//    *
+//     *
 //   *  *
-//  *    *
 // * * * * *
+
+
+for(row=1;row<=5;row++){
+    str=""
+    for(col=1;col<=5;col++){
+        if(col==row || col+row==6 || row==1 || row==5){
+            str= str+"*"
+        }
+        else{
+            str= str+" "
+        }
+    }
+   console.log(str);
+}
