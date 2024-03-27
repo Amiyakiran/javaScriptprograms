b95a994fe793c5ae94be4590fd67e21be074e4cf
Amiyakiran <amiya.susy@gmail.com>
2024-03-27 07:20:36 +0530

    Update prog33_pattern4.js
    changed the code

--- prog33_pattern4.js
+++ prog33_pattern4.js
@@ -26,7 +26,7 @@ for(row=1;row<=4;row++){
 //               *
 
 
-for(row=1;row<=4;row++){//1<=4//2<=4
+/* for(row=1;row<=4;row++){//1<=4//2<=4
     str=""//""//""
     for(sp=4;sp>=row;sp--){//4>=2//3>=2//2>=2//1>=2
         str= str+" "//####//###
@@ -47,3 +47,28 @@ for(row=0; row<=5; row++){
         }
         console.log(str);
  }    
+ */
+
+
+
+for(row=1;row<=5;row++){
+    str=''
+    for(spa=5;spa>row;spa--){
+        str=str+' '
+    }
+    for(col=1;col<=row;col++){
+        str= str+'* '
+    }
+    console.log(str);
+}
+
+for(row=4;row>=1;row--){
+    str=''
+    for(spa=5;spa>row;spa--){
+        str=str+' '
+    }
+    for(col=1;col<=row;col++){
+        str= str+'* '
+    }
+    console.log(str);
+}
