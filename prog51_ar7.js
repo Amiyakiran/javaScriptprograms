da45a0cb8fd80d64b1ba0244583ca896ce0f5157
Amiyakiran <amiya.susy@gmail.com>
2024-04-04 06:44:16 +0530

    Update prog51_ar7.js

--- prog51_ar7.js
+++ prog51_ar7.js
@@ -16,3 +16,35 @@ for(i=0;i<a.length;i++){
 
  !isDuplicate&&console.log('no duplication');
 
+
+
+b=[]
+ for(i=0;i<=a.length-1;i++){
+    for(j=i+1;j<=a.length-1;j++){
+        if(a[i]==a[j]){
+            isDuplicate=false
+            for(k=0;k<=b.length-1;k++){
+                if(a[i]==b[k]){
+                    isDuplicate=true
+                    break
+                }
+            }
+            if(!isDuplicate){
+                b.push(a[i])
+            }
+        }
+    }
+ }
+console.log(b);
+
+ b=[]
+ for(i=0;i<=a.length;i++){//10
+    for(j=i+1;j<=a.length;j++){
+        if(a[i]==a[j] && !b.includes(a[i])){
+           b.push(a[i]);
+            break
+        }
+    }
+ }
+console.log(b);
+
