1b924062f968f274114020203d6baaa0c52826f1
Amiyakiran <amiya.susy@gmail.com>
2024-08-16 11:49:02 +0530

    Update prog70_str1.js

--- prog70_str1.js
+++ prog70_str1.js
@@ -16,3 +16,34 @@ for(let char of characters){
 } */
 
 Array.from(sentance).filter(char=>vowels.includes(char)).forEach(item=>{console.log(item);})
+
+
+
+str='hello'
+
+console.log(str[4]);
+
+//w.a.p to check whether the given string is palindrom or not?
+//'malayalam'
+
+str='malayalam'
+
+start = 0
+end= str.length-1
+ispalindrome=true
+while(start<end){
+    if(str[start]!=str[end]){
+        ispalindrome=false
+    }
+    start++
+    end--
+}
+console.log(ispalindrome?'palindrome':'not palindrome');
+
+//another - for
+
+pal=''
+for(i=str.length-1;i>=0;i--){
+   pal += str[i]
+}
+console.log(str==pal?'palindrome':'not palindrome');
