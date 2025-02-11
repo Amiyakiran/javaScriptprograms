807553fbf2a5e0af1083a9fff830bd3444a6cd9e
Amiyakiran <amiya.susy@gmail.com>
2025-02-11 16:02:36 +0530

    Update prog29_for5.js

--- prog29_for5.js
+++ prog29_for5.js
@@ -12,3 +12,20 @@ for(i=1;i<=num1 && i<=num2; i++){
     }
 }
 console.log('The Highest common factor is :',gcd);
+
+//w.a.p to find the lcm of two given numbers
+//lcm
+//4 = 4, 8 , 12, 16 20 24 28 32 36 40
+//5= 5 10 15 20 25 30 
+num1 = 8 // 8 16 24 32 40 
+num2 = 10 // 10 20 30 40 50 60
+lcm = 0
+greater = num1>num2?num1:num2
+    for(i=greater; i<=num1*num2;i++){
+        if(i%num1==0 && i%num2==0){
+            lcm = i
+            break
+        }
+    }
+
+console.log(lcm);
