5114a89b57afd3266b0fce17a22662b2facdd568
Amiyakiran <amiya.susy@gmail.com>
2025-02-13 12:28:32 +0530

    Update prog39_fn5.js

--- prog39_fn5.js
+++ prog39_fn5.js
@@ -1,5 +1,7 @@
 //call back function
 
+//A callback function is a function that is passed as an argument to another function and is executed after that function completes its execution
+
 //function definition
 function greet(name,callback){ // name = peter callback = callme
         console.log('hai ',name); // hai peter
